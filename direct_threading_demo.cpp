98b485be5adfe6acec35b4c74bb3415b39288727
Stephen Leach <sfkleach@gmail.com>
2022-03-21 20:46:40 +0000

    Adding comments

--- direct_threading_demo.cpp
+++ direct_threading_demo.cpp
@@ -1,3 +1,18 @@
+/*
+This code demonstrates how to implement direct-threaded code using GNU C++ by
+exploiting a little-know extension - the ability to take the address of a label,
+store it in a pointer, and jump to it through the pointer.
+
+In this example, we translate Brainf*ck into threaded code, where the program
+is a sequence of abstract-machine instructions. Each instruction of the program
+is a pointer to a label that represents the code to execute, optionally
+followed by some integer data.
+
+We use Brainf*ck because it has very few instructions, which makes the crucial 
+method (Engine::runFile) fit on a single screen.
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -5,30 +20,48 @@
 
 #define DEBUG 0
 #define break_if( E ) if ( E ) break
-#define return_if( E ) if ( E ) return
 #define break_unless( E ) if (!(E)) break
 
+//  We use the address of a label to play the role of an operation-code.
+typedef void * OpCode;
+
+//  The instruction stream is mainly OpCodes but there are some
+//  integer arguments interspersed. Strictly speaking this makes this
+//  interpreter a hybrid between direct/indirect threading.
 typedef union {
-    void* opcode;
+    OpCode opcode;
     int operand;
 } instruction;
 
+//  This class is responsible for translating the stream of source code
+//  into a vector<instruction>. It is passed a mapping from characters
+//  to the addresses-of-labels, so it can plant (aka append) the exact
+//  pointer to the implementing code. 
 class CodePlanter {
-    std::ifstream input;
-    const std::map<char, void *> & instructions;
-    std::vector<instruction> & program;
-    std::vector<int> indexes;
+    std::ifstream input;                //  The source code to be read in.
+    const std::map<char, OpCode> & instructions;
+    std::vector<instruction> & program; 
+    std::vector<int> indexes;           //  Responsible for managing [ ... ] loops.
+
 public:
-    CodePlanter( std::string_view filename, const std::map<char, void *> & instructions, std::vector<instruction> & program ) :
+    CodePlanter( std::string_view filename, const std::map<char, OpCode> & instructions, std::vector<instruction> & program ) :
         input( filename.data(), std::ios::in ),
         instructions( instructions ),
         program( program )
     {}
 
+private:
     void plantChar( char ch ) {
+        //  Guard - skip characters that do not correspond to abstract machine
+        //  operations.
         auto it = instructions.find(ch);
-        return_if( it == instructions.end() );
+        if ( it == instructions.end() ) return;
+
+        //  Body
         program.push_back( { it->second } );
+        //  If we are dealing with loops, we plant the absolute index of the
+        //  operation in the program we want to jump to. This can be improved
+        //  fairly easily.
         if ( ch == '[' ) {
             indexes.push_back( program.size() );
             program.push_back( {nullptr} );
@@ -41,6 +74,7 @@ public:
         }
     }
 
+public:
     void plantProgram() {
         for (;;) {
             char ch = input.get();
@@ -54,7 +88,7 @@ public:
 typedef unsigned char num;
 
 class Engine {
-    std::map<char, void*> instructions;
+    std::map<char, OpCode> instructions;
     std::vector<instruction> program;
     std::vector<num> memory;
 public:
@@ -138,6 +172,10 @@ public:
     }
 };
 
+/*
+Each argument is the name of a Brainf*ck source file to be compiled into
+threaded coded and executed.
+*/
 int main( int argc, char * argv[] ) {
     const std::vector<std::string_view> args(argv + 1, argv + argc);
     for (auto arg : args) {
