67c01f62c34665f431894c6128ae34b81dcdb78d
Stephen Leach <sfkleach@gmail.com>
2022-03-26 13:30:25 +0000

    Experimental variants

---
+++ alt2_subroutine_threading_demo.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <string>
+
+#define DEBUG 0
+#define break_if( E ) if ( E ) break
+#define return_if( E ) if ( E ) return
+#define break_unless( E ) if (!(E)) break
+
+class Engine;
+
+typedef void (Engine::*OpCode)();
+
+typedef union Instruction {
+    OpCode opcode;
+    int operand;
+} Instruction;
+
+class CodePlanter {
+    std::ifstream input;
+    const std::map<char, OpCode> & opcode_map;
+    std::vector<Instruction> & program;
+    std::vector<int> indexes;
+
+public:
+    CodePlanter( 
+        std::string_view filename, 
+        const std::map<char, OpCode> & opcode_map, 
+        std::vector<Instruction> & program 
+    ) :
+        input( filename.data(), std::ios::in ),
+        opcode_map( opcode_map ),
+        program( program )
+    {}
+
+private:
+    void plantChar( char ch ) {
+        //  Guard
+        auto it = opcode_map.find(ch);
+        return_if( it == opcode_map.end() );
+
+        //  Body
+        program.push_back( { it->second } );
+        if ( ch == '[' ) {
+            indexes.push_back( program.size() );
+            program.push_back( {nullptr} );
+        } else if ( ch == ']' ) {
+            int end = program.size();
+            int start = indexes.back();
+            indexes.pop_back();
+            program[ start ].operand = end + 1;
+            program.push_back( { .operand={ start + 1 } } );
+        }
+    }
+
+public:
+    void plantProgram() {
+        for (;;) {
+            char ch = input.get();
+            break_unless( input.good() );
+            plantChar( ch );
+        }
+        program.push_back( { opcode_map.at('\0') } );
+    }
+};
+
+typedef unsigned char num;
+
+class Engine {
+    std::map<char, OpCode> opcode_map;
+    std::vector<Instruction> program;
+    std::vector<num> memory;
+    Instruction * program_data = nullptr;
+    num * loc = nullptr;
+    Instruction * pc;
+
+public:
+    Engine() : 
+        memory( 30000 )
+    {}
+
+public:
+    void INCR() {
+        if ( DEBUG ) std::cout << "INCR" << std::endl;
+        *loc += 1;
+    }
+
+    void DECR() {
+        if ( DEBUG ) std::cout << "DECR" << std::endl;
+        *loc -= 1;
+    }
+
+    void RIGHT() {
+        if ( DEBUG ) std::cout << "RIGHT" << std::endl;
+        loc += 1;
+    }
+
+    void LEFT() {
+        if ( DEBUG ) std::cout << "LEFT" << std::endl;
+        loc -= 1;
+    }
+
+    void PUT() {
+        num i = *loc;
+        if ( DEBUG ) std::cout << "PUT: " << (int)i << std::endl;
+        std::cout << i;
+    }
+
+    void GET() {
+        if ( DEBUG ) std::cout << "GET" << std::endl;
+        {
+            char ch;
+            std::cin.get( ch );
+            if (std::cin.good()) {
+                *loc = ch;
+            }
+        }
+    }
+
+    void OPEN() {
+        if ( DEBUG ) std::cout << "OPEN" << std::endl;
+        int n = pc++->operand;
+        if ( *loc == 0 ) {
+            this->pc = &program_data[n];
+        }
+    }
+
+    void CLOSE() {
+        if ( DEBUG ) std::cout << "CLOSE" << std::endl;
+        int n = pc++->operand;
+        if ( *loc != 0 ) {
+            this->pc = &program_data[n];
+        }
+    }
+
+    void HALT() {
+        if ( DEBUG ) std::cout << "DONE!";
+        exit( EXIT_SUCCESS );
+    }
+
+    void runFile( std::string_view filename, bool header_needed ) {
+        if ( header_needed ) {
+            std::cerr << "# Executing: " << filename << std::endl;
+        }
+
+        opcode_map = {
+            { '+', &Engine::INCR },
+            { '-', &Engine::DECR },
+            { '<', &Engine::LEFT },
+            { '>', &Engine::RIGHT },
+            { '[', &Engine::OPEN },
+            { ']', &Engine::CLOSE },
+            { '.', &Engine::PUT },
+            { ',', &Engine::GET },
+            { '\0', &Engine::HALT }
+        };
+        
+        CodePlanter planter( filename, opcode_map, program );
+        planter.plantProgram();
+
+        std::noskipws( std::cin );
+
+        this->program_data = program.data();
+        this->pc = &program_data[0];
+        loc = &memory.data()[0];
+        
+        for (;;) {
+            OpCode s = pc++->opcode;
+            (this->*s)();
+        }
+    }
+};
+
+int main( int argc, char * argv[] ) {
+    const std::vector<std::string_view> args(argv + 1, argv + argc);
+    for (auto arg : args) {
+        Engine engine;
+        engine.runFile( arg, args.size() > 1 );
+    }
+    exit( EXIT_SUCCESS );
+}
---
+++ alt_subroutine_threading_demo.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <string>
+
+#define DEBUG 0
+#define break_if( E ) if ( E ) break
+#define return_if( E ) if ( E ) return
+#define break_unless( E ) if (!(E)) break
+
+class Engine;
+
+typedef union Instruction * (Engine::*OpCode)( union Instruction * pc );
+
+typedef union Instruction {
+    OpCode opcode;
+    int operand;
+} Instruction;
+
+class CodePlanter {
+    std::ifstream input;
+    const std::map<char, OpCode> & opcode_map;
+    std::vector<Instruction> & program;
+    std::vector<int> indexes;
+
+public:
+    CodePlanter( 
+        std::string_view filename, 
+        const std::map<char, OpCode> & opcode_map, 
+        std::vector<Instruction> & program 
+    ) :
+        input( filename.data(), std::ios::in ),
+        opcode_map( opcode_map ),
+        program( program )
+    {}
+
+private:
+    void plantChar( char ch ) {
+        //  Guard
+        auto it = opcode_map.find(ch);
+        return_if( it == opcode_map.end() );
+
+        //  Body
+        program.push_back( { it->second } );
+        if ( ch == '[' ) {
+            indexes.push_back( program.size() );
+            program.push_back( {nullptr} );
+        } else if ( ch == ']' ) {
+            int end = program.size();
+            int start = indexes.back();
+            indexes.pop_back();
+            program[ start ].operand = end + 1;
+            program.push_back( { .operand={ start + 1 } } );
+        }
+    }
+
+public:
+    void plantProgram() {
+        for (;;) {
+            char ch = input.get();
+            break_unless( input.good() );
+            plantChar( ch );
+        }
+        program.push_back( { opcode_map.at('\0') } );
+    }
+};
+
+typedef unsigned char num;
+
+class Engine {
+    std::map<char, OpCode> opcode_map;
+    std::vector<Instruction> program;
+    std::vector<num> memory;
+    Instruction * program_data = nullptr;
+    num * loc = nullptr;
+
+public:
+    Engine() : 
+        memory( 30000 )
+    {}
+
+public:
+    Instruction *  INCR( Instruction * pc ) {
+        if ( DEBUG ) std::cout << "INCR" << std::endl;
+        *loc += 1;
+        return pc;
+    }
+
+    Instruction *  DECR( Instruction * pc ) {
+        if ( DEBUG ) std::cout << "DECR" << std::endl;
+        *loc -= 1;
+        return pc;
+    }
+
+    Instruction *  RIGHT( Instruction * pc ) {
+        if ( DEBUG ) std::cout << "RIGHT" << std::endl;
+        loc += 1;
+        return pc;
+    }
+
+    Instruction *  LEFT( Instruction * pc ) {
+        if ( DEBUG ) std::cout << "LEFT" << std::endl;
+        loc -= 1;
+        return pc;
+    }
+
+    Instruction * PUT( Instruction * pc ) {
+        num i = *loc;
+        if ( DEBUG ) std::cout << "PUT: " << (int)i << std::endl;
+        std::cout << i;
+        return pc;
+    }
+
+    Instruction * GET( Instruction * pc ) {
+        if ( DEBUG ) std::cout << "GET" << std::endl;
+        {
+            char ch;
+            std::cin.get( ch );
+            if (std::cin.good()) {
+                *loc = ch;
+            }
+        }
+        return pc;
+    }
+
+    Instruction * OPEN( Instruction * pc ) {
+        if ( DEBUG ) std::cout << "OPEN" << std::endl;
+        int n = pc++->operand;
+        if ( *loc == 0 ) {
+            pc = &program_data[n];
+        }
+        return pc;
+    }
+
+    Instruction * CLOSE( Instruction * pc ) {
+        if ( DEBUG ) std::cout << "CLOSE" << std::endl;
+        int n = pc++->operand;
+        if ( *loc != 0 ) {
+            pc = &program_data[n];
+        }
+        return pc;
+    }
+
+    Instruction * HALT( Instruction * pc ) {
+        if ( DEBUG ) std::cout << "DONE!";
+        exit( EXIT_SUCCESS );
+        return pc;
+    }
+
+    void runFile( std::string_view filename, bool header_needed ) {
+        if ( header_needed ) {
+            std::cerr << "# Executing: " << filename << std::endl;
+        }
+
+        opcode_map = {
+            { '+', &Engine::INCR },
+            { '-', &Engine::DECR },
+            { '<', &Engine::LEFT },
+            { '>', &Engine::RIGHT },
+            { '[', &Engine::OPEN },
+            { ']', &Engine::CLOSE },
+            { '.', &Engine::PUT },
+            { ',', &Engine::GET },
+            { '\0', &Engine::HALT }
+        };
+        
+        CodePlanter planter( filename, opcode_map, program );
+        planter.plantProgram();
+
+        std::noskipws( std::cin );
+
+        this->program_data = program.data();
+        Instruction * pc = &program_data[0];
+        loc = &memory.data()[0];
+        
+        for (;;) {
+            OpCode s = pc++->opcode;
+            pc = (this->*s)( pc );
+        }
+    }
+};
+
+int main( int argc, char * argv[] ) {
+    const std::vector<std::string_view> args(argv + 1, argv + argc);
+    for (auto arg : args) {
+        Engine engine;
+        engine.runFile( arg, args.size() > 1 );
+    }
+    exit( EXIT_SUCCESS );
+}
